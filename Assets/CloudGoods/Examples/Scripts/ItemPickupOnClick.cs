237db09c97f4e2de181080b6ff3dc19e58900e78
Lionel Sy <lionely.sy@gmail.com>
2014-12-04 12:20:47 -0500

    added itempickonclick for item aquire scene

---
+++ Assets/CloudGoods/Examples/Scripts/ItemPickupOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickupOnClick : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                hit.collider.gameObject.GetComponent<ItemDataComponent>().Pickup(true);
+            }
+        }
+	}
+}
---
+++ Assets/TestNewItemGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestNewItemGenerator : MonoBehaviour {
+
+    void Start()
+    {
+        CloudGoods.OnRegisteredUserToSession += OnUserRegisteredToSession;
+    }
+
+    void OnUserRegisteredToSession(string seessionID)
+    {
+        CloudGoods.NewGenerateItems(10, 300, OnReceviedGenerationResponse);
+    }
+
+    void OnReceviedGenerationResponse(GeneratedItems generatedItems)
+    {
+        Debug.Log("Received generation items count: " + generatedItems.generatedItems.Count + "  GenerationID: " + generatedItems.GenerationID);
+
+        List<SelectedGenerationItem> giveItems = new List<SelectedGenerationItem>();
+
+        foreach(ItemData item in generatedItems.generatedItems)
+        {
+            Debug.Log("start giving item to user: ID: " + item.ItemID);
+
+            SelectedGenerationItem selectItem = new SelectedGenerationItem();
+
+            selectItem.ItemId = item.ItemID;
+            selectItem.Amount = item.stackSize;
+
+            giveItems.Add(selectItem);
+        }
+
+        CloudGoods.GiveGeneratedItemToOwner("User", giveItems, generatedItems.GenerationID, 0, OnReceivedGiveItemGenerationResponse);
+    }
+
+    void OnReceivedGiveItemGenerationResponse(List<GiveGeneratedItemResult> giveItemResults)
+    {
+
+    }
+
+
+}
